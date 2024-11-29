63bd8b6627d7f3d6192d78e694d1c4db80843118
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-29 13:37:33 -0300

    Evitando duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -7,9 +7,20 @@ export default function useOrder() {
 
     // Functions
     const addItem = (item : MenuItem) => {
-        const newItem : OrderItem = {...item, quantity: 1}
-        setOrder([...order, newItem])
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist) {
+            const updatedOrder = order.map ( orderItem => orderItem.id === item.id ?
+                {...orderItem, quantity: orderItem.quantity + 1} :
+                orderItem
+            )
+            setOrder(updatedOrder)
+        } else {
+            const newItem : OrderItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
     }
+    console.log(order)
+
     return {
         addItem
     }
