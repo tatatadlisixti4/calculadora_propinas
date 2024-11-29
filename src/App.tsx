489f750d6082432dc52d81d83c6f14a6f497d3db
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-28 23:35:59 -0300

    Creando el Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ function App() {
 
     return (
         <>
-            <h1>Hola</h1>
+            <header className="bg-teal-400 py-5">
+                <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+            </header>
         </>
     )
 }
