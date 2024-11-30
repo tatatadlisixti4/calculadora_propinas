11444421510a9a33180b1dd7eee2083d2468920f
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-30 16:44:35 -0300

    Calculando el consumo y otros parámetros

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,9 @@ function App() {
                         order={order}
                         removeItem={removeItem}
                     />
-                    <OrderTotals />
+                    <OrderTotals
+                        order={order}
+                    />
                 </div>
 
 
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,18 +1,31 @@
-export default function OrderTotals() {
+import {OrderItem} from "../types";
+import {useMemo} from "react";
+import {formatCurrency} from "../helpers";
+
+type OrderTotalsProps = {
+    order: OrderItem[]
+}
+
+export default function OrderTotals({order}: OrderTotalsProps) {
+    const subtotalAmount = useMemo(() =>
+        order.reduce((total, item) => total + (item.price * item.quantity) ,0)
+        , [order]
+    )
+
     return (
         <>
             <div className="space-y-3">
                 <h2 className="font-black text-2xl">Totales y Propina:</h2>
                 <p>Subtotal a pagar: {''}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
                 </p>
 
                 <p>Propina: {''}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">{formatCurrency(0)}</span>
                 </p>
 
                 <p>Total a pagar: {''}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">{formatCurrency(0)}</span>
                 </p>
             </div>
 
